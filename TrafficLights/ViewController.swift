cd2512e9d69b1039bf57faac3015a1a6d9918ea0
Alex Zhulai <Lepiozdyx@gmail.com>
2023-03-17 17:14:01 -0300

    Setting ViewController logic

--- TrafficLights/ViewController.swift
+++ TrafficLights/ViewController.swift
@@ -9,11 +9,42 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var redLightView: UIView!
+    @IBOutlet weak var yellowLightView: UIView!
+    @IBOutlet weak var greenLightView: UIView!
+    
+    @IBOutlet weak var startButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        cornerRadiusSet(for: redLightView)
+        cornerRadiusSet(for: yellowLightView)
+        cornerRadiusSet(for: greenLightView)
+        
+        startButton.layer.cornerRadius = 10
     }
-
-
+    
+    @IBAction func pushingButton(_ sender: UIButton) {
+        startButton.setTitle("NEXT", for: .normal)
+        
+        if redLightView.alpha < 1 && yellowLightView.alpha < 1 && greenLightView.alpha < 1 {
+            redLightView.alpha = 1
+        } else if redLightView.alpha == 1 {
+            redLightView.alpha = 0.3
+            yellowLightView.alpha = 1
+        } else if yellowLightView.alpha == 1 {
+            yellowLightView.alpha = 0.3
+            greenLightView.alpha = 1
+        } else if greenLightView.alpha == 1 {
+            greenLightView.alpha = 0.3
+            redLightView.alpha = 1
+        }
+    }
+    
+    private func cornerRadiusSet(for view: UIView) {
+        view.layer.cornerRadius = 75
+    }
+    
 }
 
