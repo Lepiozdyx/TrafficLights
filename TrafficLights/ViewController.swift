4a16596d8889f2d412fb264ccb9b4dc033140ca9
Alex Zhulai <Lepiozdyx@gmail.com>
2023-03-18 13:55:06 -0300

    change ViewController class name

--- TrafficLights/ViewController.swift
+++ TrafficLights/ViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
 
     @IBOutlet weak var redLightView: UIView!
     @IBOutlet weak var yellowLightView: UIView!
